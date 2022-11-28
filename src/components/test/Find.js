3df35c3f75c228349b4390d38f535ca1ccde203d
ETU936 <80760102+ToavinaM@users.noreply.github.com>
2022-11-28 17:20:02 +0300

    Delete src/components/test directory

--- src/components/test/Find.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-
-export default function Find({ methodFind }) {
-
-    return (
-        <input onChange={(rep) => methodFind(rep.target.value)}></input >
-    )
-}
